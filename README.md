da65eaf689e0925440764374e2b41b498aa2500e
Technion <technion@lolware.net>
2018-08-21 12:22:35 +0000

    Workable, installable version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AzureEnum
 
-This Ruby Gem assists in enumeration of Office 365 federated domains. This can allow you to identify domains associated with a business, not easily identified through traditional means.
+This Ruby Gem assists in enumeration of Office 365 or Exchange on-premise federated domains. This can allow you to identify domains associated with a business, not easily identified through traditional means. The examples below demonstrate how output can be interesting.
 
 The time this process takes can vary from a few seconds to a few minutes depending on the hosting server.
 ## Installation
@@ -37,6 +37,44 @@ lolzware.onmicrosoft.com
 lolware.net
 ```
 
+## Examples
+
+The following examples against some random domains demonstrate the tools capabilities.
+
+```
+$ azure_enum afl.com.au
+Please wait while the given domain is enumerated.
+afl.com.au
+aflnt.com.au
+ntthunder.com.au
+aflgoulburnmurray.com.au
+aflwesterndistrict.com.au
+aflgippsland.com.au
+aflyarraranges.com.au
+
+$ azure_enum kmart.com.au
+Please wait while the given domain is enumerated.
+kmart.com.au
+KASAsia.com
+
+$ azure_enum microsoft.com
+Please wait while the given domain is enumerated.
+corp.webtv.net
+microsoft.onmicrosoft.com
+surface.com
+bungie.com
+navic.tv
+middleeast.corp.microsoft.com
+wingroup.windeploy.ntdev.microsoft.com
+exchangecalendarsharing.com
+redmond.corp.microsoft.com
+northamerica.corp.microsoft.com
+bing.com
+corp.microsoft.com
+placeware.com
+(snip large list)
+```
+
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake test` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
@@ -46,6 +84,11 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 ## Contributing
 
 Bug reports and pull requests are welcome on GitHub at https://github.com/technion/azure_enum.
+Sometimes you get this output:
+
+    Unknown key: Max-Age = 31536000
+
+It seems to be a known bug in HTTPClient.
 
 ## License
 
--- lib/azure_enum.rb
+++ lib/azure_enum.rb
@@ -3,7 +3,9 @@ require "erb"
 require "httpclient"
 require "nokogiri"
 
+# Azure and Exchange federated domain enumerator
 module AzureEnum
+  # Class initializes with a domain name, and provides methods to interact with MS Autodiscover
   class Federation
     def initialize(domain)
       @domain = domain
@@ -11,6 +13,7 @@ module AzureEnum
       @redirect = nil
     end
 
+    # This will identify if the http:// redirect exists for the domain, usually per Office 365
     def check_redirect
       url = "http://autodiscover.#{@domain}/autodiscover/autodiscover.svc"
       begin
@@ -24,8 +27,8 @@ module AzureEnum
 
     def enumerate_autodisc
       httpsdomains = [
-        "https://#{@domain}/autodiscover/autodiscover.svc",
-        "https://autodiscover.#{@domain}/autodiscover/autodiscover.svc"
+        "https://autodiscover.#{@domain}/autodiscover/autodiscover.svc",
+        "https://#{@domain}/autodiscover/autodiscover.svc"
       ]
 
       httpsdomains.unshift @redirect if @redirect
@@ -36,48 +39,58 @@ module AzureEnum
           content = { "Content-Type" => "text/xml; charset=utf-8" }
           res = http.post(url, xml, content)
           @xml_text = res.content
-          return @xml_text
-          last
+          return true
+          # It is bad style to rescue "all" errors. However, it turns out there is a practically
+          # never ending list of ways this can fail. And "any" failure is reason to rule out the address
         rescue
           next
         end
       end
+      return false
     end
+
     def getdomains
-      fail "enumumerate_autodisc not called yet" unless @xml_text
-       tree = Nokogiri.parse(@xml_text)
-       tree.xpath(
-         "//ad:GetFederationInformationResponseMessage/ad:Response/ad:Domains/ad:Domain",
-         "ad": "http://schemas.microsoft.com/exchange/2010/Autodiscover")
-         .map do |node|
-            node.text
-       end
+      raise "enumumerate_autodisc not called yet" unless @xml_text
+      tree = Nokogiri.parse(@xml_text)
+      tree.xpath(
+        "//ad:GetFederationInformationResponseMessage/ad:Response/ad:Domains/ad:Domain",
+        ad: "http://schemas.microsoft.com/exchange/2010/Autodiscover"
+      ).map(&:text)
     end
 
     private
+
+    # This is an internal class just to pass the correct structure to ERB in get_xml
     class Discovery
       def initialize(domain, url)
         @domain = domain
         @url = url
       end
+
       def get_binding
         binding
       end
     end
+
     def get_xml(domain, url)
       path = File.dirname __dir__
-      template = File.read(File.join path, "discovery.xml.erb")
+      template = File.read(File.join(path, "discovery.xml.erb"))
       renderer = ERB.new(template)
       discovery = Discovery.new(domain, url)
       renderer.result(discovery.get_binding)
     end
   end
+
+  # This is the intended API: runs each step of the enumeration process and returns a result
   class << self
     def federated(domain)
       e = Federation.new(domain)
       e.check_redirect
-      e.enumerate_autodisc
-      e.getdomains
+      if e.enumerate_autodisc
+        e.getdomains
+      else
+        nil
+      end
     end
   end
 end
